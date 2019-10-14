a9e16c9f14fec07583758c017901552cac092f12
Guillermo <thecrow88@gmail.com>
2019-10-14 21:28:03 +0200

    First commit

---
+++ testlibrary/src/main/java/com/hyperdimension/testlibrary/MyEditText.kt
@@ -0,0 +1,26 @@
+package com.hyperdimension.testlibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+
+class MyEditText : EditText {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+
+}
